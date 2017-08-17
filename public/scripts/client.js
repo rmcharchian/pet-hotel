bde5f69b0701aa0aa9a33df55446fe65e6877873
haiphi <ahodges230@gmail.com>
2017-08-17 18:27:06 -0500

    Created controller and sourced in angular and client.js

---
+++ public/scripts/client.js
@@ -0,0 +1,6 @@
+var app = angular.module('PetHotelApp', []);
+
+app.controller('PetHotelController', function() {
+    console.log('Pet Hotel Controller has been loaded');
+    var self = this;
+});
