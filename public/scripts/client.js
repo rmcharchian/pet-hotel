9696d30b4db46165bcafb5d3692eaeb19bbbf552
haiphi <ahodges230@gmail.com>
2017-08-18 13:47:15 -0500

    Added GET route

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,19 @@
 var app = angular.module('PetHotelApp', []);
 
-app.controller('PetHotelController', function() {
+app.controller('PetHotelController', ['$http', function ($http) {
     console.log('Pet Hotel Controller has been loaded');
-    var self = this;
-});
+
+    self.pets = [];
+    self.owners = [];
+
+
+    self.getPets = function(){
+       $http({
+            method: 'GET',
+            url: '/petRoutes'
+        }).then(function (response) {
+            console.log(response.data);
+            self.pets = response.data;
+        }); // end of $http
+    }; // end of getMessages
+}]);//appController
