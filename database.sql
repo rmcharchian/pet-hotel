7b4bc2f3a0a6f995a015d6d444a215c018be51b4
Canin Apriori <canin@dreamfreely.org>
2017-08-18 14:17:24 -0500

    populating databases and added FK from Pet to Owner, also removed owner_id from visits table

--- database.sql
+++ database.sql
@@ -10,13 +10,14 @@ CREATE TABLE pets (
 id SERIAL PRIMARY KEY,
 pet_name VARCHAR(30)NOT NULL,
 breed VARCHAR(30)NOT NULL,
-color VARCHAR(30)NOT NULL
+color VARCHAR(30)NOT NULL,
+owner_id INT REFERENCES owners
 );
 
 CREATE TABLE visits (
 check_in_date DATE,
 check_out_date DATE,
-owners_id INT REFERENCES owners,
+-- owners_id INT REFERENCES owners,
 pets_id INT REFERENCES pets
 );
 
@@ -27,3 +28,6 @@ VALUES ('John', 'Boberton'),
 INSERT INTO pets(pet_name, breed, color)
 VALUES ('Wilbur', 'collie', 'grey'),
 ('BamBam', 'labrador', 'black');
+
+INSERT INTO visits(check_in_date, check_out_date, pets_id)
+VALUES ('2016-3-5', '2016-3-20', 1), ('2016-5-3', '2016-5-20', 1);
