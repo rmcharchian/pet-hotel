2871a46bc78d1d742b742958b54044ce16e04552
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-08-18 08:22:25 -0500

    added some routes to the routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,5 +4,25 @@ var router = express.Router();
 var pool = require('/routes/pool');
 
 
+
+route.get('/', function (){
+    console.log('Get route hit');
+    
+});
+
+route.post('/', function (){
+    console.log('Get route hit');
+    
+});
+
+route.put('/', function (){
+    console.log('Get route hit');
+    
+});
+
+route.delete('/', function (){
+    console.log('Get route hit');
+    
+});
 //gotta send it somewhere
 module.exports = router;
